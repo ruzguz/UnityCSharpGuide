031bf8624aa50dd83192ca5eb40e02b9a6120dd7
Rusben Guzman <rusbenguzman@gmail.com>
2021-05-01 14:03:53 -0400

    Challenge #1: create basic variables and print in the console

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Variable to store name
+    public string  name = "Rusben"; 
+    // Variable to store Age
+    public int age = 25;
+    // Variable to store speed
+    public float speed = 5f;
+    // Health variable
+    public int health = 3;
+    // Score variable 
+    // Check is all keys were collected
+    public bool hasAllKeys = false;
+    // Ammo count vairable 
+    public int ammo = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Name: " + name);
+        Debug.Log("Age: " + age);
+        Debug.Log("Speed: " + speed);
+        Debug.Log("Health: " + health);
+        Debug.Log("Player has all keys?: " + hasAllKeys);
+        Debug.Log("Ammo: " + ammo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
