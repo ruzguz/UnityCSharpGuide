08e9f3fa2ff59ae76ee09c1f2f858444f9355d5d
Rusben Guzman <rusbenguzman@gmail.com>
2021-05-03 17:52:34 -0400

    Create script to check if someone have the age to drive

---
+++ Assets/Scripts/IfStatement/PlayerIfStatement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIfStatement : MonoBehaviour
+{
+    public int myAge = 16;
+    private int _legalAgeToDrive = 16;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // if my age is greather or equal than 16 (legal age to drive)
+        // then print "Hey you can drive"
+        // else
+        // print "you CAN NOT drive"
+
+        if (myAge >= _legalAgeToDrive) 
+        {
+            Debug.Log("Hey!!! You can drive");
+        }
+        else 
+        {
+            Debug.Log("You CAN NOT drive");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
