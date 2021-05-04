5c448381314f1770406e4391f5f9730f7d104470
Rusben Guzman <rusbenguzman@gmail.com>
2021-05-04 19:34:25 -0400

    Create difficulty selection with else-if statement

---
+++ Assets/Scripts/ElseIf/PlayerElseIf.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerElseIf : MonoBehaviour
+{
+
+    // Difficulty vars
+    public int easyDifficulty = 0;
+    public int mediumDifficulty = 1;
+    public int hardDifficulty = 2;
+    public int currentDifficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDifficulty = mediumDifficulty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Checking selected difficulty
+        if (currentDifficulty == easyDifficulty) 
+        {
+            Debug.Log("You selected easy level");
+        }
+        else if (currentDifficulty == mediumDifficulty)
+        {
+            Debug.Log("You selected medium level");
+        }
+        else if (currentDifficulty == hardDifficulty) 
+        {
+            Debug.Log("You selected hard level");
+        }
+        else 
+        {
+            Debug.Log("Invalid Level");
+        }
+    }
+}
