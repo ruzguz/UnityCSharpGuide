4e31e162f46ce0472ae8b581f1cc18360bf4994a
Rusben Guzman <rusbenguzman@gmail.com>
2021-05-04 20:32:49 -0400

    Challenge: check player speed and print a message

---
+++ Assets/Scripts/ElseIf/SpeedController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedController : MonoBehaviour
+{
+
+    // Script to increment and decrement player speed
+
+    public int speed = 0;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Moving cube
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        // if press A then increase the speed
+        // else if press S drecrease the speed
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            speed += 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            speed = Mathf.Clamp(speed - 2, 0, int.MaxValue);
+        }
+
+        // if speed is greather than 20 then print slow down
+        // else if speed is equals to 0 then print speed up
+
+        if (speed > 20)
+        {
+            Debug.Log("Slow Down");
+        }
+        else if (speed == 0)
+        {
+            Debug.Log("Speed Up!!!");
+        }
+    }
+}
