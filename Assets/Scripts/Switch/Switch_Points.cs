2e7ee95184ea246c294baf5b5beb02034302aa75
Rusben Guzman <rusbenguzman@gmail.com>
2021-05-31 21:46:34 -0400

    Master switch challenge 1

---
+++ Assets/Scripts/Switch/Switch_Points.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch_Points : MonoBehaviour
+{
+
+    [SerializeField]
+    private int _points = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            _points = 50;
+        } 
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            _points = 100;
+        } 
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            _points = 0;
+        }
+
+        switch(_points)
+        {
+            case 50:
+                Debug.Log("Points are 50!");
+                break;
+            case 100:
+                Debug.Log("Points are 100");
+                break;
+            default:
+                Debug.Log("No points :(");
+                break;
+        }
+    }
+}
